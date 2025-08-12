01e9de50b73b539d49c23ce162da9bcb49e14d96
BeboBonbo <bebobonbo111165@gamil.com>
2025-08-12 08:00:04 +0200

    Object State

--- challenges-react/src/App.js
+++ challenges-react/src/App.js
@@ -1,63 +1,66 @@
-import Post from "./posts.js";
-import NavBar from "./navbar.js";
-import BtnSideSec from "./btnSideSection.js";
-import "./App.css";
+// import Post from "./posts.js";
+// import NavBar from "./navbar.js";
+// import BtnSideSec from "./btnSideSection.js";
+// import "./App.css";
+import Scoreboard from "./objectStateProblemInDocs";
 
-const showCategories = true;
+// const showCategories = true;
 
 function App() {
-  const bodyStyle = {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "start",
-    gap: 16,
-    width: "60%",
-  };
-  const posts = [
-    {
-      id: 1,
-      title: "M&H",
-      content: "Features: FrontEnd - BackEnd - AI - MobileApp",
-    },
-    { id: 2, title: "Hello World", content: "This is the hello world article" },
-    { id: 3, title: "Post 3", content: "This is the body of post 3" },
-    { id: 4, title: "This is the post title", content: "This is the post body" },
-    { id: 5, title: "This is the post title", content: "This is the post body" },
-    { id: 6, title: "This is the post title", content: "This is the post body" },
-    { id: 7, title: "Post 7", content: "This is the body of post 7" },
-  ];
-  const postsComponents = posts.map((post) => {
-    return <Post key={post.id} title={post.title} content={post.content} />;
-  });
+  return <Scoreboard />;
 
-  return (
-    <div className="App">
-      <NavBar />
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <section style={bodyStyle}>
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              gap: "1rem",
-              width: "70%",
-            }}
-          >
-            {postsComponents}
-          </div>
-          <AppSideMenu />
-        </section>
-      </div>
-    </div>
-  );
+  // const bodyStyle = {
+  //   display: "flex",
+  //   justifyContent: "center",
+  //   alignItems: "start",
+  //   gap: 16,
+  //   width: "60%",
+  // };
+  // const posts = [
+  //   {
+  //     id: 1,
+  //     title: "M&H",
+  //     content: "Features: FrontEnd - BackEnd - AI - MobileApp",
+  //   },
+  //   { id: 2, title: "Hello World", content: "This is the hello world article" },
+  //   { id: 3, title: "Post 3", content: "This is the body of post 3" },
+  //   { id: 4, title: "This is the post title", content: "This is the post body" },
+  //   { id: 5, title: "This is the post title", content: "This is the post body" },
+  //   { id: 6, title: "This is the post title", content: "This is the post body" },
+  //   { id: 7, title: "Post 7", content: "This is the body of post 7" },
+  // ];
+  // const postsComponents = posts.map((post) => {
+  //   return <Post key={post.id} title={post.title} content={post.content} />;
+  // });
+
+  // return (
+  //   <div className="App">
+  //     <NavBar />
+  //     <div style={{ display: "flex", justifyContent: "center" }}>
+  //       <section style={bodyStyle}>
+  //         <div
+  //           style={{
+  //             display: "flex",
+  //             flexDirection: "column",
+  //             gap: "1rem",
+  //             width: "70%",
+  //           }}
+  //         >
+  //           {postsComponents}
+  //         </div>
+  //         <AppSideMenu />
+  //       </section>
+  //     </div>
+  //   </div>
+  // );
 }
 
-function AppSideMenu() {
-  if (showCategories == true) {
-    return <BtnSideSec />;
-  } else {
-    return null;
-  }
-}
+// function AppSideMenu() {
+//   if (showCategories == true) {
+//     return <BtnSideSec />;
+//   } else {
+//     return null;
+//   }
+// }
 
 export default App;
