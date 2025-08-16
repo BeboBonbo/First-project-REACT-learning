ee8e8845dc6e79ec2237bcd2eace7821736e6e63
BeboBonbo <bebobonbo111165@gamil.com>
2025-08-16 08:17:04 +0200

    Using (useContext) to share data in the APP

--- First-project-inReactCourse/src/App.js
+++ First-project-inReactCourse/src/App.js
@@ -2,6 +2,7 @@ import FormRequest from "./FormRequest";
 import { UserContext } from "./contexts/UserContext";
 function App() {
   return (
+    // use context.provider
     <UserContext.Provider value = {{userName: "Bonbo", name: "Habiba", email: "Bonbo@gmail.com"}} >
       <div className="App" style={{background: "rgb(10, 0, 53)", width: "100%", height: "fitContent", padding: "3rem 0", display: "flex", alignItems: "center", justifyContent: "center"}}>
         <FormRequest />
