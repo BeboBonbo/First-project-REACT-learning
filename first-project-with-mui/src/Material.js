da221ef9336cf1329055915b3261d19497d788a1
BeboBonbo <bebobonbo111165@gamil.com>
2025-08-18 07:36:06 +0200

    Material UI Challenge

--- first-project-with-mui/src/Material.js
+++ first-project-with-mui/src/Material.js
@@ -46,22 +46,84 @@
 //   );
 // }
 
-import Button from "@mui/material/Button";
-import DeleteIcon from "@mui/icons-material/Delete";
-import SendIcon from "@mui/icons-material/Send";
-import Stack from "@mui/material/Stack";
-import AddToPhotosIcon from '@mui/icons-material/AddToPhotos';
+//! import Button from "@mui/material/Button";
+//! import DeleteIcon from "@mui/icons-material/Delete";
+//! import SendIcon from "@mui/icons-material/Send";
+//! import Stack from "@mui/material/Stack";
+//! import AddToPhotosIcon from '@mui/icons-material/AddToPhotos';
 
-export default function IconLabelButtons() {
+//! export default function IconLabelButtons() {
+//!   return (
+//!     <Stack direction="row" spacing={2}>
+//!         <AddToPhotosIcon style={{color: "red", fontSize: "40px"}} />
+//!       <Button variant="outlined" startIcon={<DeleteIcon />}>
+//!         Delete
+//!       </Button>
+//!       <Button variant="contained" endIcon={<SendIcon />}>
+//!         Send
+//!       </Button>
+//!     </Stack>
+//!   );
+//! }
+
+import * as React from "react";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import Container from "@mui/material/Container";
+import Switch from "@mui/material/Switch";
+import Collapse from "@mui/material/Collapse";
+
+const label = { inputProps: { "aria-label": "Switch demo" } };
+
+export default function DisabledAccordion() {
+  const [checked, setChecked] = React.useState(true);
   return (
-    <Stack direction="row" spacing={2}>
-        <AddToPhotosIcon style={{color: "red", fontSize: "40px"}} />
-      <Button variant="outlined" startIcon={<DeleteIcon />}>
-        Delete
-      </Button>
-      <Button variant="contained" endIcon={<SendIcon />}>
-        Send
-      </Button>
-    </Stack>
+    <Container maxWidth="md" style={{ marginTop: "200px" }}>
+      <div>
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1-content"
+            id="panel1-header"
+          >
+            <Typography component="span">Accordion 1</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+              eget.
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel2-content"
+            id="panel2-header"
+          >
+            <Typography component="span">Accordion 2</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Switch
+              {...label}
+              value={checked}
+              onChange={() => {
+                setChecked((prev) => !prev);
+              }}
+              defaultChecked
+            />
+          </AccordionDetails>
+        </Accordion>
+      </div>
+      <Collapse in={checked} collapsedSize={100}>
+        <div style={{ height: "400px", background: "orange" }}>
+          <h1>Hello World</h1>
+        </div>
+      </Collapse>
+    </Container>
   );
 }
